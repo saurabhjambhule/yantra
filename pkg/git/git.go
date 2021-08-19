68283ab4d39c34b81f1c484b0a6fa62bd65e718d
Saurabh Jambhule <sjambhule@g2.com>
2021-08-19 17:08:12 +0530

    get default git branch

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -3,10 +3,13 @@ package git
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
+
+const remoteRef string = "refs/remotes/origin/"
 
 func GetCurrentRepo() *git.Repository {
 	dir, err := os.Getwd()
@@ -50,6 +53,34 @@ func GetBranch() string {
 	}
 
 	return currentBranch
+}
+
+func GetDefaultBranch() string {
+	repository := GetCurrentRepo()
+
+	refs, err := repository.References()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var dafaultBranch string
+	err = refs.ForEach(func(ref *plumbing.Reference) error {
+		// The HEAD is omitted in a `git show-ref` so we ignore the symbolic
+		// references, the HEAD
+		if ref.Type() == plumbing.SymbolicReference {
+			return nil
+		}
+
+		if strings.HasPrefix(ref.Name().String(), remoteRef) {
+			dafaultBranch = ref.Name().String()
+		}
+
+		return nil
+	})
+
+	dafaultBranch = strings.TrimPrefix(dafaultBranch, remoteRef)
+
+	return dafaultBranch
 }
 
 func GetCommit() string {
